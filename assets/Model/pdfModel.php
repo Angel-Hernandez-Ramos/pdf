3a0417c4ce6295e922d4ab8adc8d8537a6509330
Angel <angel.hernandezra@alumno.buap.mx>
2023-03-12 14:30:35 -0600

    fin del formatt

--- assets/Model/pdfModel.php
+++ assets/Model/pdfModel.php
@@ -16,7 +16,7 @@ class Generador{
        
         $alignx = 30;
         $ancho = 100;
-        $fill = 1;
+        $fill = 0;
         $pdf->setXY($alignx,27);
         $pdf->MultiCell($ancho, 5,"PACIENTE: {$data["nombrep"]}",$fill,'L',0);
         $pdf->SetFont('Helvetica','B',8);
@@ -83,36 +83,75 @@ class Generador{
         $pdf->MultiCell($ancho, 2,"Sab 7:30 a 16:00:",$fill,'C',0);
         $pdf->setX($alignx);
         $pdf->MultiCell($ancho, 2,"Dom 7:30 a 16:00:",$fill,'C',0);
+        $pdf->Ln(7);
 
-       
-
-        // Metodo para generar Tabla
-        $alt = $pdf->GetY();
-        $pdf->setXY(10,$alt+10);
-        $pdf->SetFont('Helvetica','',7);
-        $tamcolumns = [10,40,105,20,20];
-        $tamfil=3.4;
-        foreach ($data["info"] as $key => $value) {
-            
-            $rows = [];
-            $spacex = [];
-            foreach ($value as $index => $val) {
-                $spacex[] = $tamcolumns[$index];
-                $rows[] = $pdf->MultiCell($tamcolumns[$index], $tamfil,"$val",1,'J',0);
-                $pdf->setY($pdf->GetY() - $rows[sizeof($rows)-1]*$tamfil);
-                $pdf->setX($pdf->GetX() + (array_sum($spacex)));
-            }
-            $rengMax[] = max($rows);
-            $pdf->setXY(10,$alt+10);
-            // var_dump(array_sum($rengMax));
-            $pdf->Ln(array_sum($rengMax)*$tamfil);
-        }
+        $titles[0] = ["CANT","","DESCRIPCION","PRECIO","TOTAL"];
+        $alt = $this->GenerarTable($pdf,$titles,[10,40,105,20,20],3.4,"B");
+        $pdf->Line(8,$pdf->GetY(), 208, $pdf->GetY());
+        $this->GenerarTable($pdf,$data["info"],[10,40,105,20,20],3.4,"");
         
-        $pdf->RoundedRect(8, $alt+3, 200, 60, 3.5, 'D');
+        $pdf->RoundedRect(8, $alt-3, 200, 60, 3.5, 'D');
+        $pdf->setY(60+$alt);
+
+        $altG =$pdf->GetY();
+        $pdf->SetFont('Helvetica','',14);
+        $rowsLin[] = $pdf->MultiCell(40, 4,"      I      I",0,'L',0);
+        $pdf->Line(8,$pdf->GetY(), 50, $pdf->GetY());
+        $pdf->SetFont('Helvetica','',5);
+        $rowsLin[] = $pdf->MultiCell(40, 4,"Fecha Y Hora de Entrega:",0,'C',0);
+        $pdf->SetFont('Helvetica',B'',8);
+        $rowsLin[] = $pdf->MultiCell(40, 4,"PACIENTE:",0,'C',0);
+        $pdf->SetFont('Helvetica','',5);
+        $rowsLin[] = $pdf->MultiCell(40, 4,"Metodo de Pago:",0,'C',0);
+        $pdf->SetFont('Helvetica','B',8);
+        $rowsLin[] = $pdf->MultiCell(40, 4,"Efectivo",0,'C',0);
+
+        $pdf->Line(50,$altG, 50, $altG+(array_sum($rowsLin)*4));
+
+        $pdf->SetFont('Helvetica','',8);
+        $pdf->setXY(50,$altG);
+        $pdf->MultiCell(110, 4,"Cantidad en Moneda\nUn mil pecitos",0,'l',0);
+
+        $pdf->RoundedRect(8, $altG, 153, 20, 2, 'D');
+
+        $pdf->setXY(165,$altG);
+        // $pdf->setX(165);
+        $pdf->MultiCell(40, 4,"Total:\t 100",0,'l',0);
+        $pdf->setX(165);
+        $pdf->MultiCell(40, 4,"Anticipo:\t 300",0,'l',0);
+        $pdf->setX(165);
+        $pdf->MultiCell(40, 4,"Restante:\t 34345345s",0,'l',0);
+        $pdf->setX(165);
+
+        $pdf->MultiCell(40, 4,"PAGADO",0,'C',0);
+        $pdf->RoundedRect(165, $altG, 43, 20, 2, 'D');
+
         $pdf->Close();
         $encode64 = base64_encode($pdf->Output('S'));
         return ["estatus"=>"ok","data"=>$encode64];
         // $pdf->Output('I');
-    } 
+    }
+    public function GenerarTable($thiss,$data,$tamcolumns,$tam,$letter){
+        $alt = $thiss->GetY();
+        $thiss->setXY(10,$alt);
+        $thiss->SetFont('Helvetica',"{$letter}",7);
+        $tamcolumns = $tamcolumns;
+        $tamfil=$tam;
+        foreach ($data as $key => $value) {
+            $rows = [];
+            $spacex = [];
+            foreach ($value as $index => $val) {
+                $spacex[] = $tamcolumns[$index];
+                $rows[] = $thiss->MultiCell($tamcolumns[$index], $tamfil,"$val",0,'J',0);
+                $thiss->setY($thiss->GetY() - $rows[sizeof($rows)-1]*$tamfil);
+                $thiss->setX($thiss->GetX() + (array_sum($spacex)));
+            }
+            $rengMax[] = max($rows);
+            $thiss->setXY(10,$alt);
+            // var_dump(array_sum($rengMax));
+            $thiss->Ln(array_sum($rengMax)*$tamfil);
+        }
+        return $alt;
+    }
 }
 ?>
